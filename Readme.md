20b594bcdb1705a676de4f9a50c8c4b88d7109f0
Ivan Sahumbaiev <hadhoryth@gmail.com>
2025-07-22 13:28:00 +0200

    Batman

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Development environment for terminal usage inspired by [ThePrimeagen](https://github.com/ThePrimeagen/dev).
+Installed with custom configuration:
+ - nvim
+ - tmux
+ - fzf
---
+++ env/.config/nvim/init.lua
@@ -0,0 +1,15 @@
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Key mappings
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
